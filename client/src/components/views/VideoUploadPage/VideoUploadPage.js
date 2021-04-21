befea7b88972b8eef325a86c9b8d18270a61c711
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-21 14:59:54 +0900

    * input / textarea / section onchange setting

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Typography, Button, Form, message, Input, Icon} from 'antd';
 import Dropzone from 'react-dropzone';
 
 const { Title } = Typography;
 const { TextArea } = Input;
 
+const PrivateOptions = [
+    {value: 0, label: "Private"},
+    {value: 1, label: "Public"}
+]
+
+const CategoryOptions = [
+    {value: 0, label: "Film & Animation"},
+    {value: 1, label: "Autos & Vehicles"},
+    {value: 2, label: "Music"},
+    {value: 3, label: "Pets & Animals"}
+]
+
 function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState("");
+    const [Description, setDescription] = useState("");
+    const [Private, setPrivate] = useState(0);
+    const [Category, setCategory] = useState("Film & Animation");
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value);
+    }
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value);
+    }
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value);
+    }
+
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value);
+    }
+
     return (
         <div style={{maxWidth:'700px', margin:'2rem auto'}}>
             <div style={{textAlign:'center', marginBottom:'2rem'}}>
@@ -30,26 +64,34 @@ function VideoUploadPage() {
                         <img src alt />
                     </div>
                 </div>
+
                 <br />
                 <br />
                 <label>Title</label>
-                <Input onChange value />
+                <Input onChange={onTitleChange} value={VideoTitle} />
+                
                 <br />
                 <br />
                 <label>Description</label>
-                <TextArea onChange value />
+                <TextArea onChange={onDescriptionChange} value={Description} />
+                
                 <br />
                 <br />
-                <select onChange>
-                    <option key value></option>
+                <select onChange={onPrivateChange}>
+                    {PrivateOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
                 </select>
+                
                 <br />
                 <br />
-                <select onChange>
-                    <option key value></option>
-                </select>
+                <select onChange={onCategoryChange}>
+                    {CategoryOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}                </select>
                 <br />
                 <br />
+                
                 <Button type="primary" size="large" onClick>
                     Submit
                 </Button>
